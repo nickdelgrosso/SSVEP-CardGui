11690eb0a766bb351c9bbb76c3afe907625726c6
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2021-07-21 21:34:04 +0200

    added trial dict

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from psychopy import visual, core
+from psychopy import visual, core, data
 from psychopy.hardware.keyboard import Keyboard
 
 from blinking_image import BlinkingImage
@@ -7,10 +7,17 @@ keyboard = Keyboard()
 
 win = visual.Window(size=(1024, 768), fullscr=True, screen=0, winType='pyglet', units='height')
 
+trials = data.TrialHandler(
+    trialList=[
+        {'freqLeft': 5, 'freqMiddle': 10, 'freqRight': 20}
+    ],
+    nReps=1
+)
+
 images = [
-    BlinkingImage(win=win, blink_frequency=5, image='imgs/card5.png', pos=(-0.25, 0), size=(0.25, 0.25)),
-    BlinkingImage(win=win, blink_frequency=10, image='imgs/card5.png', pos=(-0.0, 0), size=(0.25, 0.25)),
-    BlinkingImage(win=win, blink_frequency=20, image='imgs/card5.png', pos=(0.25, 0), size=(0.25, 0.25)),
+    BlinkingImage(win=win, blink_frequency=trials.trialList[0]['freqLeft'], image='imgs/card5.png', pos=(-0.5, 0), size=(0.25, 0.25)),
+    BlinkingImage(win=win, blink_frequency=trials.trialList[0]['freqMiddle'], image='imgs/card5.png', pos=(0.0, 0), size=(0.25, 0.25)),
+    BlinkingImage(win=win, blink_frequency=trials.trialList[0]['freqRight'], image='imgs/card5.png', pos=(0.5, 0), size=(0.25, 0.25)),
 ]
 
 while True:
