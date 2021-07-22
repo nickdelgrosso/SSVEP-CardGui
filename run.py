1fc35023ede3b04931f6c9aa8f499f1f26582bc8
Nour Chahine <nourchahine2010@gmail.com>
2021-07-22 11:08:52 -0400

    Added title and different cards, updated frequencies

--- run.py
+++ run.py
@@ -5,13 +5,15 @@ from blinking_image import BlinkingImage
 
 keyboard = Keyboard()
 
+
 win = visual.Window(size=(1024, 768), fullscr=True, screen=0, winType='pyglet', units='height')
 
+title = visual.TextStim(win, text="Pick a Card!", pos = (0, 0.25))
 
 trials = data.TrialHandler(
     trialList=[
         # {'freqLeft': 3, 'freqMiddle': 5, 'freqRight': 8},
-        {'freqLeft': 4.3, 'freqMiddle': 6, 'freqRight': 7.5},
+        {'freqLeft': 7.5, 'freqMiddle': 4.3, 'freqRight': 6.66},
     ],
     nReps=1,
     extraInfo={'duration': 20},
@@ -25,9 +27,9 @@ trials = data.TrialHandler(
 logger = logging.LogFile(f="stimlog.txt", level=logging.INFO, filemode='w')
 
 images = [
-    BlinkingImage(win=win, image='imgs/card5.png', pos=(-0.5, 0), size=(0.25, 0.25), name="leftCard"),
-    BlinkingImage(win=win, image='imgs/card5.png', pos=(0.0, 0), size=(0.25, 0.25), name="middleCard"),
-    BlinkingImage(win=win, image='imgs/card5.png', pos=(0.5, 0), size=(0.25, 0.25), name="rightCard"),
+    BlinkingImage(win=win, image='imgs/card2.png', pos=(-0.5, -0.20), size=(0.25, 0.341), name="leftCard"),
+    BlinkingImage(win=win, image='imgs/card7.png', pos=(0.0, -0.2), size=(0.25, 0.341), name="middleCard"),
+    BlinkingImage(win=win, image='imgs/cardA.png', pos=(0.5, -0.2), size=(0.25, 0.341), name="rightCard"),
 ]
 
 for trial in trials:
@@ -42,6 +44,7 @@ for trial in trials:
         for image in images:
             image.update_draw()
             image.draw()
+        title.draw()
         win.flip()  # refresh the screen
         
         if keyboard.getKeys(keyList=["escape"]):  # escape key to quit
